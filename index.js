4af0c3605d5674edd73f10525e4113f0748f70f7
boltun_oreh <alazankin@gmail.com>
2020-06-22 19:58:31 +0300

    add chords, fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = micro(async (req, res) => {
 
     const response = session.new
         ? replies.welcome()
-        : checkAnswer(sessionState, request.command);
+        : checkAnswer(sessionState, request.nlu, request.command);
 
     return {
         response,
@@ -18,13 +18,22 @@ const server = micro(async (req, res) => {
 
 /**
  * @param sessionState
+ * @param nlu
  * @param command
  *
  *  @returns {{tts: string, buttons: [*], end_session: boolean, text: string}|{tts: string, end_session: boolean, text: string}|{end_session: boolean, text: string}}
  */
-function checkAnswer(sessionState, command) {
-    if (/что ты умеешь/i.test(command) || /помощь/i.test(command)) {
+function checkAnswer(sessionState, nlu, command) {
+    if ('YANDEX.WHAT_CAN_YOU_DO' in nlu.intents || 'YANDEX.HELP' in nlu.intents) {
         return replies.help('');
+    }
+    
+    if ('song_info' in nlu.intents) {
+        if (nlu.intents.song_info.slots.what.value === 'аккорды') {
+            return replies.songChords(nlu.intents.song_info.slots.song.value);
+        } else if (nlu.intents.song_info.slots.what.value === 'текст') {
+            return replies.songText(nlu.intents.song_info.slots.song.value);
+        }
     }
 
     if (/перечислить песни/i.test(command)) {
--- src/replies.js
+++ src/replies.js
@@ -2,24 +2,24 @@
  * Welcome message
  */
 exports.welcome = () => {
-  const greeting = getRandomElement(['Привет', 'Здравствуйте']);
+    const greeting = getRandomElement(['Привет', 'Здравствуйте']);
 
-  return {
-    text: `${greeting}! Я - ваш виртуальный путеводитель по творчеству группы Фикус Генри. Если что-то нужно - только попросите.`,
-    buttons: [
-      {title: 'Что ты умеешь?', hide: true},
-    ],
-    end_session: false
-  };
+    return {
+        text: `${greeting}! Я - ваш виртуальный путеводитель по творчеству группы Фикус Генри. Если что-то нужно - только попросите.`,
+        buttons: [
+            {title: 'Что ты умеешь?', hide: true},
+        ],
+        end_session: false
+    };
 };
 
 exports.invalid = () => {
-    return help('Простите, я вам не поняла.')
+    return this.help('Простите, я вас не поняла.')
 };
 
 exports.help = (preText) => {
     return {
-        text: `${preText} ${getHelpText()}`,
+        text: `${preText} ${this.getHelpText()}`,
         buttons: [
             {title: 'Перечислить песни', hide: true},
             {title: 'Напомнить аккорды', hide: true},
@@ -37,39 +37,77 @@ exports.getHelpText = () => {
 
 exports.listSongs = () => {
     return {
-        text: `Про наркоманов. Рай. Н1. Питер. МТЖ. Доктор. Алкоголь. Дура.`, // todo randomize
+        text: `Про наркоманов. Рай. Н1. Питер. МТЖ. Доктор. Алкоголь. Дура.`, // todo move to classes, add different names
         end_session: false
     };
+};
+
+/**
+ * @param {String} songName
+ *
+ * @returns {{tts: *, end_session: boolean, text: *}}
+ */
+exports.songChords = (songName) => {
+    let chords, chordsCount, tts, errorText;
+
+    switch (songName) {
+        case 'про наркоманов':
+            chords = 'H C';
+            chordsCount = 2;
+            break;
+        case 'рай':
+            chords = 'D B(6)';
+            chordsCount = 2;
+            break;
+        case 'алкоголь':
+            chords = 'E F E H C G A';
+            chordsCount = 6;
+            break;
+        default:
+            errorText = 'Не могу знать, ведь такая песня еще не написана.'
+    }
+
+    if (errorText) {
+        return this.reply(errorText);
+    }
+
+    if (chordsCount === 2) {
+        tts = 'Это изи, их всего два. Как и во всех остальных песнях. ' + chords;
+    } else if (chordsCount > 2 && chordsCount < 5) {
+        tts = 'Тут стоит сосредоточиться, ведь их аж ' + chordsCount + '. Но я верю, ты справишься. ' + chords;
+    } else {
+        tts = 'Не знаю, готова ли ты, ведь если бы аккорды были ... а ..., то потребовось бы ... .' + chords;
+    }
+
+    return this.reply(chords, tts);
+};
+
+exports.songText = (songName) => {
+    
 };
 
 /**
  * Reply
  *
  * @param {String} text
+ * @param {String} tts
  */
-exports.reply = (text) => {
+exports.reply = (text, tts) => {
     return {
-        text: `${text}`,
+        text: text,
+        tts: tts,
         end_session: false
     };
 };
 
 /**
- * Повторяем фразу пользователя.
+ * 
+ * @param arr
  *
- * @param {String} command
+ *  @returns {*}
  */
-exports.repeatUserCommand = command => {
-  return {
-    text: `Может это ты ${command}?`,
-    tts: `Это ты ${command}, а я - Дадон!!<speaker audio="alice-sounds-game-loss-2.opus">`,
-    buttons: [capitulateButton],
-    end_session: false
-  };
-};
-
 function getRandomElement(arr) {
-  const index = Math.floor(Math.random() * arr.length);
+    const index = Math.floor(Math.random() * arr.length);
 
-  return arr[index];
+    return arr[index];
 }
