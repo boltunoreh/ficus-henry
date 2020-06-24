e39fab53567afb564a24aff30bf7dbe44ce68887
boltun_oreh <alazankin@gmail.com>
2020-06-24 16:45:34 +0300

    use express instead of micro, add mongodb

--- index.js
+++ index.js
@@ -1,51 +1,93 @@
-const micro = require('micro');
-const replies = require('./src/replies');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const replies = require('./lib/replies');
+const dbConfig = require('./config/db');
 
-const server = micro(async (req, res) => {
-    const {request, session, state} = await micro.json(req);
-    const sessionState = state && state.session || {};
+const app = express();
+const jsonParser = express.json();
 
-    const response = session.new
-        ? replies.welcome()
-        : checkAnswer(sessionState, request.nlu, request.command);
+let dbClient;
 
-    return {
-        response,
-        session_state: sessionState,
-        version: '1.0'
-    };
+MongoClient.connect(dbConfig.url, function (err, client) {
+    if (err) return console.log(err);
+    dbClient = client;
+    app.locals.collection = client.db("heroku_85klzkss").collection("songs");
+
+    let port = process.env.PORT;
+    if (port == null || port == '') {
+        port = 3000;
+    }
+
+    app.listen(port, function () {
+        console.log(`Server started on http://localhost:${port}`);
+    });
 });
 
-/**
- * @param sessionState
- * @param nlu
- * @param command
- *
- *  @returns {{tts: string, buttons: [*], end_session: boolean, text: string}|{tts: string, end_session: boolean, text: string}|{end_session: boolean, text: string}}
- */
-function checkAnswer(sessionState, nlu, command) {
-    if ('YANDEX.WHAT_CAN_YOU_DO' in nlu.intents || 'YANDEX.HELP' in nlu.intents) {
-        return replies.help('');
-    }
-    
-    if ('song_info' in nlu.intents) {
-        if (nlu.intents.song_info.slots.what.value === 'аккорды') {
-            return replies.songChords(nlu.intents.song_info.slots.song.value);
-        } else if (nlu.intents.song_info.slots.what.value === 'текст') {
-            return replies.songText(nlu.intents.song_info.slots.song.value);
+app.post("/", jsonParser, function (request, response) {
+    let command = request.body.request.command;
+    let intents = request.body.request.nlu.intents;
+            
+    let text = '';
+
+    // let songRepository = require('./lib/repository/song-repository');
+
+    try {
+        const collection = request.app.locals.collection;
+
+        if ('song_info' in intents) {
+            let song_info = intents.song_info.slots;
+            let songName = song_info.song.value;
+            let song = collection.findOne({"name": songName}, {}, (err, item) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (song_info.what.value === 'аккорды') {
+                        response.send({
+                            response: replies.reply(item.chords),
+                            // session_state: sessionState,
+                            version: '1.0'
+                        });
+                    } else if (song_info.what.value === 'текст') {
+                        response.send({
+                            response: replies.reply(item.text),
+                            // session_state: sessionState,
+                            version: '1.0'
+                        });
+                    } else {
+                        response.send({
+                            response: replies.invalid(),
+                            // session_state: sessionState,
+                            version: '1.0'
+                        });
+                    }
+                }
+            });
+
+            let e = song;
         }
+
+        if (/перечислить песни/i.test(command)) {
+            // let songs = songRepository.getSongs(db);
+            let songs = collection.find({});
+
+            let songNames = [];
+            songs.each(function (err, item) {
+                if (item != null) {
+                    songNames.push(item.name);
+                } else {
+                    text = songNames.join(', ');
+
+                    response.send({
+                        response: replies.reply(text),
+                        // session_state: sessionState,
+                        version: '1.0'
+                    });
+                }
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        dbClient.close();
     }
-
-    if (/перечислить песни/i.test(command)) {
-        return replies.listSongs();
-    }
-
-    return replies.invalid();
-}
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
-
-server.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+});
